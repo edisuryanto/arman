73badc7bf3829f6fd0eae45a1d72f6e76fa7c1fa
EDI SURYANTO <lerengngunung2@gmail.com>
2025-11-13 09:30:01 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -12,6 +12,10 @@ let socks5Address = '';
 let enableSocks = false;
 let enableHttp = false;
 const expire = false;
+const horse = "dHJvamFu";
+const flash = "dm1lc3M=";
+const v2 = "djJyYXk=";
+const neko = "Y2xhc2g=";
 let proxyIPs;
 let socks5s;
 let go2Socks5s = [
